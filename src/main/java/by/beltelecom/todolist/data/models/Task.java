2dcc8e42583c8fc95cb78b6ddd7eabc6dbb70935
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2022-08-12 11:14:07 +0300

    Рабочий TaskController

--- src/main/java/by/beltelecom/todolist/data/models/Task.java
+++ src/main/java/by/beltelecom/todolist/data/models/Task.java
@@ -14,7 +14,7 @@ import java.util.Date;
 public class Task {
 
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column
     private String name;
---
+++ src/main/java/by/beltelecom/todolist/web/controllers/TaskController.java
@@ -0,0 +1,52 @@
+package by.beltelecom.todolist.web.controllers;
+
+import by.beltelecom.todolist.data.models.Task;
+import by.beltelecom.todolist.services.tasks.TasksService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@Controller
+public class TaskController {
+
+    @Autowired
+    private TasksService tasksService; // Tasks service bean;
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskController.class);
+
+    @GetMapping("/task")
+    public String getTaskPage(@RequestParam(value = "id") long aId) {
+
+
+
+        return "task";
+    }
+
+    @GetMapping("/create-task")
+    public ModelAndView getCreateTaskPage() {
+        ModelAndView mav = new ModelAndView("create-task");
+        mav.addObject("task", Task.newTask());
+
+        return mav;
+    }
+
+    @PostMapping("/create-task")
+    public String createTask(@ModelAttribute("task") Task aTask, HttpServletRequest request) {
+        LOGGER.warn("Task name:" +aTask.getName());
+        LOGGER.warn("Task desc:" +aTask.getDescription());
+        aTask.setDateCreation(new Date());
+        Task createdTask = this.tasksService.createTask(aTask);
+
+        return "redirect:/task?id=" +createdTask.getId();
+    }
+
+
+}
