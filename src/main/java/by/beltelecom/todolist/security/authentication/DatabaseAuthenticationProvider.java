be260c1b7701d971adae09ff985f0d130f516f04
VyacheslavSaniuk <saniuk.vyacheslav.97@gmail.com>
2022-08-17 22:56:48 +0300

    Create DatabaseAuthenticationProvider. Need to create DatabaseAuthenticationManager, and custom UsedDetailsService

---
+++ src/main/java/by/beltelecom/todolist/security/authentication/DatabaseAuthenticationProvider.java
@@ -0,0 +1,22 @@
+package by.beltelecom.todolist.security.authentication;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
