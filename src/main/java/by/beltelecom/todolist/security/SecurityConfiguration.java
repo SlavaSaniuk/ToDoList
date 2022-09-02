1dfa113e529173f22e1e67ab831900c41ff1ccd5
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2022-09-02 15:09:56 +0300

    Worked CORS configuration

--- src/main/java/by/beltelecom/todolist/security/SecurityConfiguration.java
+++ src/main/java/by/beltelecom/todolist/security/SecurityConfiguration.java
@@ -26,6 +26,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Objects;
 
@@ -97,6 +99,25 @@ public class SecurityConfiguration {
             http.addFilterBefore(this.jsonWebTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
             return http.build();
+        }
+
+        /**
+         * Configure web mvc configuration.
+         * @return {@link WebMvcConfigurer} bean.
+         */
+        @Bean
+        public WebMvcConfigurer corsConfiguration() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry
+                            .addMapping("/rest/**")
+                            .allowedOrigins("*")
+                            .allowedHeaders("Authorization");
+
+                    WebMvcConfigurer.super.addCorsMappings(registry);
+                }
+            };
         }
 
     }
