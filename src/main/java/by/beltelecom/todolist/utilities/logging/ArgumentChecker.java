79e9a6c870507603bac2326d9cccff201059aee3
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2022-10-20 15:03:55 +0300

    Added ArgumentChecker checker class

---
+++ src/main/java/by/beltelecom/todolist/utilities/logging/ArgumentChecker.java
@@ -0,0 +1,17 @@
+package by.beltelecom.todolist.utilities.logging;
+
+import by.beltelecom.todolist.data.models.Identification;
+
+import java.util.Objects;
+
+public abstract class ArgumentChecker {
+
+    public static void nonNull(Object aObj, String anArgName) {
+        Objects.requireNonNull(aObj, Checks.argumentNotNull(anArgName, aObj.getClass()));
+    }
+
+    public static void idNotZero(Identification aObj)  {
+        if ( aObj.getIdentifier().longValue() == 0)
+            throw new IllegalArgumentException(String.format("Required property[id] in argument of class [%s] must be not null.", aObj.getClass()));
+    }
+}
