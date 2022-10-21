1aa201f669570aaa307fbe8104fc03e8930d1e5e
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2022-10-21 09:32:07 +0300

    Tested TaskRestController#deleteUserTask method

--- src/test/java/by/beltelecom/todolist/integration/web/rest/tasks/TaskRestControllerTestsCase.java
+++ src/test/java/by/beltelecom/todolist/integration/web/rest/tasks/TaskRestControllerTestsCase.java
@@ -104,4 +104,56 @@ public class TaskRestControllerTestsCase {
         Assertions.assertNotNull(restDto);
         Assertions.assertFalse(restDto.isException());
     }
+
+    @Test
+    void deleteUserTask_taskIsNotExist_shouldReturnExceptionDtoWithStatusCode669() throws Exception {
+        // Create user and task:
+        TestingUser testingUser = this.testsUserService.testingUser("deleteUserTask2");
+
+        // Create request:
+        MvcResult mvcResult = this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/rest/task/delete-task")
+                                .param("id", "333")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .header("Authorization", testingUser.authentication().getAuthorizationHeaderValue()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String responseJson = mvcResult.getResponse().getContentAsString();
+        LOGGER.debug("Response JSON: " +responseJson);
+
+        ExceptionRestDto restDto = this.objectMapper.readValue(responseJson, ExceptionRestDto.class);
+        Assertions.assertNotNull(restDto);
+        Assertions.assertTrue(restDto.isException());
+        Assertions.assertEquals(669, restDto.getExceptionCode());
+    }
+
+    @Test
+    void deleteUserTask_userTryToDeleteNotOwnTask_shouldReturnExceptionDtoWithStatusCode604() throws Exception {
+        // Create user and task:
+        TestingUser testingUser = this.testsUserService.testingUser("deleteUserTask3");
+        TestingUser testingUser2 = this.testsUserService.testingUser("deleteUserTask4");
+        Task createdTask = this.testsTaskService.testTask(testingUser.getUser());
+
+
+        // Create request:
+        MvcResult mvcResult = this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/rest/task/delete-task")
+                                .param("id", String.valueOf(createdTask.getId()))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .header("Authorization", testingUser2.authentication().getAuthorizationHeaderValue()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String responseJson = mvcResult.getResponse().getContentAsString();
+        LOGGER.debug("Response JSON: " +responseJson);
+
+        ExceptionRestDto restDto = this.objectMapper.readValue(responseJson, ExceptionRestDto.class);
+        Assertions.assertNotNull(restDto);
+        Assertions.assertTrue(restDto.isException());
+        Assertions.assertEquals(604, restDto.getExceptionCode());
+        LOGGER.debug(restDto.getExceptionMessage());
+    }
 }
