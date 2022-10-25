b05eac6214017dca1245c207bf823fe333d8cb7d
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2022-10-25 14:32:27 +0300

    Added JavaDocs to OwnerChecker service bean.

--- src/main/java/by/beltelecom/todolist/services/security/owning/OwnerChecker.java
+++ src/main/java/by/beltelecom/todolist/services/security/owning/OwnerChecker.java
@@ -4,8 +4,19 @@ import by.beltelecom.todolist.data.models.User;
 import by.beltelecom.todolist.exceptions.NotFoundException;
 import by.beltelecom.todolist.exceptions.security.NotOwnerException;
 
+/**
+ * Type safe security service bean user to check if user own T entity object.
+ * @param <T> - Entity type.
+ */
 public interface OwnerChecker<T> {
 
+    /**
+     * Check if user own specified entity object.
+     * @param aUser - user owner.
+     * @param aT - any entity object.
+     * @throws NotOwnerException - Throws in cases when user not own entity object.
+     * @throws NotFoundException - Throws in cases when specified entity object not exist in database.
+     */
     void isUserOwn(User aUser, T aT) throws NotOwnerException, NotFoundException;
 
 }
