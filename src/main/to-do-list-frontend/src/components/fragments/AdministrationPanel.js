06034eba3aa8da041f895aae95fadeebdeeee596
VyacheslavSaniuk <saniuk.vyacheslav.97@gmail.com>
2022-11-03 22:29:23 +0300

    Worked AdministrationPanel.js rendering based on application user roles.

--- src/main/to-do-list-frontend/src/components/fragments/AdministrationPanel.js
+++ src/main/to-do-list-frontend/src/components/fragments/AdministrationPanel.js
@@ -3,18 +3,41 @@ import {Logging} from "../../js/utils/Logging";
 import '../../styles/fragments/AdministrationPanel.css'
 import {ReqUtilities} from "../utilities/ReqUtilities";
 
+/**
+ * AdministrationBlock is root component of AdministrationPanel fragment that check render or do not render
+ * AdministrationPanel. If application user has administration roles - ApplicationPanel will be rendered (see
+ * ComponentDidMount function).
+ * Component props:
+ * @property gl_applicationUSer - application user global variable;
+ */
 class AdministrationBlock extends React.Component {
 
+    /**
+     * Construct new react component.
+     * Constructor set initial component state.
+     * @param props - component props.
+     */
     constructor(props) {
         super(props);
 
         //Set initial state:
         this.state = ({
-            isRender: false
+            isRender: false, // Rendering flag;
+            userRoles: [] // Array of user roles;
         })
     }
 
+    /**
+     * Check if application user has administration roles, and if it has - render this component (set state isRender
+     * flag to true). Send get request on "/rest/administration/hello" url and if response status code is 200 - user
+     * has administration roles (Spring security work), else if response code <> 200 (403) - user doesn't have
+     * administration roles.
+     * If user have administration roles, function get it's by sending get request on "/rest/administration/user-roles"
+     * url. Then map fetched roles to state "userRoles" property and render this component.
+     */
     componentDidMount() {
+
+        const fetchedUserRoles = []; // Fetched roles:
 
         // Create fetch request to check if application user has admin roles:
         fetch(ReqUtilities.absoluteUrl("/rest/administration/hello"), {
@@ -27,8 +50,26 @@ class AdministrationBlock extends React.Component {
         }).then(result => {
             if (result.ok) {
                 Logging.log("Hello admin user: ", this.props.gl_applicationUser.loadApplicationUser().userId);
-                // Render this component:
-                this.setState({isRender: true});
+
+                // Get user roles:
+                ReqUtilities.getRequest("/rest/administration/user-roles").then(response => {
+                    if (response.ok) {
+                        // Get response body (list of user roles):
+                        response.json().then((listOfUserRoles => {
+                            // Add user roles to user roles list:
+                            listOfUserRoles.forEach(userRole => fetchedUserRoles.push(userRole));
+
+                            // Render component:
+                            this.setState(prevState => {
+                                // Return new state:
+                                return {
+                                    isRender: true,
+                                    userRoles: prevState.userRoles.concat(fetchedUserRoles)
+                                }
+                            });
+                        }))
+                    }
+                })
             }else {
                 Logging.log("User is not admin!");
                 this.setState({isRender: false});
@@ -50,6 +91,7 @@ class AdministrationBlock extends React.Component {
 
         return (
             <div className={"admin-block"}>
+                <p> Administration panel </p>
 
             </div>
         );
